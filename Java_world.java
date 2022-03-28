c32cdaf759654965d70e12526ed54ed1aace8b3d
nitin3103 <101620268+nitin3103@users.noreply.github.com>
2022-03-28 11:02:02 +0530

    Add files via upload

---
+++ Java_world.java
@@ -0,0 +1,15 @@
+package Self_Evaluation_10;
+
+public class Java_world {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   int a=15,k=0,i=1;
+   while(k<a)
+  {
+	System.out.println(i+" Java_World");
+	k++;
+	i++;
+	}
+	}
+   }
